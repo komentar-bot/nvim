c4580f28facc1d7cfe7c2bf1129462d1fdb4c49e
777uliahshafar <777uliahshafar@gmail.com>
2023-06-30 06:58:34 +0700

    add

---
+++ lua/core/luasnips.lua
@@ -0,0 +1,41 @@
+if not pcall(require, "luasnip") then
+  return
+end
+
+local ls = require "luasnip"
+
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/lua/snippets"})
+
+ls.config.set_config {
+  -- This tells LuaSnip to remember to keep around the last snippet.
+  -- You can jump back into it even if you move outside of the selection
+  history = false,
+
+  -- This one is cool cause if you have dynamic snippets, it updates as you type!
+  updateevents = "TextChanged,TextChangedI",
+
+  -- Autosnippets:
+  enable_autosnippets = true,
+}
+
+-- <c-j> is my expansion key
+-- this will expand the current item or jump to the next item within the snippet.
+vim.keymap.set({ "i", "s" }, "<c-l>", function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
+end, { silent = true })
+
+-- <c-k> is my jump backwards key.
+-- this always moves to the previous item within the snippet
+vim.keymap.set({ "i", "s" }, "<c-h>", function()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
+end, { silent = true })
+
+vim.keymap.set("i", "<c-j>", function()
+  if ls.choice_active() then
+    ls.change_choice(1)
+  end
+end)
