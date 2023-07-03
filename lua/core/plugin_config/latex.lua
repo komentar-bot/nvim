1a631d2bd507bd2d2bc4959e06857fa4df8e1351
777uliahshafar <777uliahshafar@gmail.com>
2023-07-03 16:10:45 +0700

    obviouslywarningattempt

--- lua/core/plugin_config/latex.lua
+++ lua/core/plugin_config/latex.lua
@@ -12,3 +12,6 @@ vim.g.vimtex_compiler_latexmk = {
         "-synctex=1",
       },
     }
+
+vim.g.vimtex_view_general_viewer = "SumatraPDF"
+vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
