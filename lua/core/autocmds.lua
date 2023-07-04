d073542a094b078429939515d4a9734ca1805e6c
777uliahshafar <777uliahshafar@gmail.com>
2023-07-04 10:25:50 +0700

    foldexpr

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -14,11 +14,12 @@ local autocmd = vim.api.nvim_create_autocmd   -- Create autocommand
 -- vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
 ---WORKAROUND
 vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
-  group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
-  callback = function()
-    vim.opt.foldmethod     = 'expr'
-    vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
-  end
+    pattern = {"*.lua"},
+    group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
+    callback = function()
+        vim.opt.foldmethod     = 'expr'
+        vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
+    end
 })
 ---ENDWORKAROUND
 
