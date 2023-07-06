4ea5d37d14136b2658e5adb2e0a8788187c997cc
777uliahshafar <777uliahshafar@gmail.com>
2023-07-06 18:45:04 +0800

    fbbgcolor

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -15,7 +15,9 @@ end, {})
         palette_overrides = {
             light0_hard = "#ebdbb2",
             light1 = "#f8f8f2",
-            dark1= "#1d2021",
+            light2= "#f8f8f2", 
+            dark1= "#3c3836", 
+            dark2= "#3c3836", 
             dark4 = "#a89984",
         },
         overrides = {
