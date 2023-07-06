5be372be346a0231c15eedf6a9d426caee302356
777uliahshafar <777uliahshafar@gmail.com>
2023-07-06 09:18:47 +0800

    fixcolor

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -15,14 +15,13 @@ end, {})
         palette_overrides = {
             light0_hard = "#ebdbb2",
             light1 = "#f8f8f2",
-            dark1= "#3c3836",
+            dark1= "#1d2021",
             dark4 = "#a89984",
         },
         overrides = {
             SignColumn = {bg = "#a89984"},
             CursorLine = {bg = "NONE"},
-            CursorLineNr = {bg = "NONE"},
-
+            CursorLineNr = {bg = "NONE", fg= "#d65d0e"},
         }
       })
 
