6ea7162b6f53259650f6b45611a1311a7f4de4df
777uliahshafar <777uliahshafar@gmail.com>
2023-07-07 08:56:22 +0800

    schemeline

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -12,18 +12,6 @@ end, {})
 
       require("gruvbox").setup({
         contrast = "hard",
-        palette_overrides = {
-            light0_hard = "#fbf1c7",
-            light1 = "#f8f8f2",
-            light2= "#f8f8f2",
-            dark1= "#3c3836",
-            dark2= "#1B1D1E",
-            dark4 = "#a89984",
-        },
-        overrides = {
-            CursorLine = {bg = "NONE"},
-            CursorLineNr = {bg = "NONE", fg= "#d65d0e"},
-        }
       })
 
 vim.cmd[[colorscheme gruvbox]]
