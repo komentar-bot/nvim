d57ce793e67325449a8145041769c777bf5277b5
777uliahshafar <777uliahshafar@gmail.com>
2023-07-13 15:46:06 +0800

    potentiallagneorg

--- lua/core/plugin_config/norg.lua
+++ lua/core/plugin_config/norg.lua
@@ -1,24 +1,8 @@
-local neorg_callbacks = require("neorg.callbacks")
-
-neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
-    -- Map all the below keybinds only when the "norg" mode is active
-    keybinds.map_event_to_mode("norg", {
-        n = { -- Bind keys in normal mode
-            { "<C-s>", "core.integrations.telescope.find_linkable" },
-            { "<leader>c", "core.qol.todo_items.todo.task_cycle" },
-        },
-        i = { -- Bind in insert mode
-            { "<C-l>", "core.integrations.telescope.insert_link" },
-        },
-    }, {
-        silent = true,
-        noremap = true,
-    })
-end)
-
       require("neorg").setup {
         load = {
           ["core.defaults"] = {}, -- Loads default behaviour
+
+          ["core.concealer"] = {},
           ["core.highlights"] = {
             config = {
                 highlights = {
@@ -32,19 +16,12 @@ end)
                     },
                 },
         ["core.integrations.telescope"] = {},
-        ["core.autocommands"] = {},
         ["core.integrations.treesitter"] = {},
-        ["core.presenter"] = {
-          config = {
-            zen_mode = "zen-mode",
-          },
-        },
-        ["core.integrations.zen_mode"] = {},
           ["core.dirman"] = { -- Manages Neorg workspaces
             config = {
               workspaces = {
                 aca = "~/notes/aca",
-                archiplus = "~/notes/archiplus",
+                archi = "~/notes/archi",
               },
             default_workspace = "aca",
             },
@@ -68,11 +45,14 @@ template = {
                         },
                     },
         ["core.integrations.nvim-cmp"] = {},
-        ["core.export"] = {},
-        ["core.export.markdown"] = {
-          config = {
-            extensions = "all",
-          },
-        },
+["core.keybinds"] = {
+    config = {
+        hook = function(keybinds)
+keybinds.remap_event("norg", "n", "<C-s>", "core.integrations.telescope.find_linkable")
+keybinds.remap_event("norg", "n", "<leader>c", "core.qol.todo_items.todo.task_cycle")
+keybinds.remap_event("norg", "i", "<C-l>", "core.integrations.telescope.insert_link")
+end,
+}
+}
         },
       }
