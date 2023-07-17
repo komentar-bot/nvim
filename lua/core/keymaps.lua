3f4f27e01e149e59f0019a77fd2d0701f903272b
777uliahshafar <777uliahshafar@gmail.com>
2023-07-17 23:51:38 +0800

    telebuffer

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -51,7 +51,7 @@ vim.api.nvim_set_keymap('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', 
 --  telescope
 map("n", "<leader>fr", "<CMD>Telescope oldfiles<CR>")
 map("n", "<leader>ff", "<CMD>Telescope find_files<CR>")
-map("n", "<leader>fb", "<CMD>Telescope file_browser<CR>")
+map("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
 map("n", "<leader>fw", "<CMD>Telescope live_grep<CR>")
 map("n", "<leader>ht", "<CMD>Telescope colorscheme<CR>")
 
