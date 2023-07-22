01401542096a50c67f3e53a773b1973e1fa79cb0
777uliahshafar <777uliahshafar@gmail.com>
2023-07-22 13:41:12 +0800

    loadsnip

--- lua/core/snippets.lua
+++ lua/core/snippets.lua
@@ -4,7 +4,8 @@ end
 
 local ls = require("luasnip")
 
-require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "./snippets" } })
+require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_snipmate").lazy_load()
 
 -- Config
 ls.config.set_config {
