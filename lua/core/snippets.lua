2d199806a177ba9944570b3ad79a43bbddbcbdc2
777uliahshafar <777uliahshafar@gmail.com>
2023-07-22 13:30:06 +0800

    cleanluasnip

--- lua/core/snippets.lua
+++ lua/core/snippets.lua
@@ -3,45 +3,12 @@ if not pcall(require, "luasnip") then
 end
 
 local ls = require("luasnip")
--- some shorthands...
-local s = ls.snippet
-local sn = ls.snippet_node
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
-local c = ls.choice_node
-local d = ls.dynamic_node
-local l = require("luasnip.extras").lambda
-local r = require("luasnip.extras").rep
-local p = require("luasnip.extras").partial
-local m = require("luasnip.extras").match
-local n = require("luasnip.extras").nonempty
-local dl = require("luasnip.extras").dynamic_lambda
-local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
-local types = require("luasnip.util.types")
-local conds = require("luasnip.extras.expand_conditions")
-local pi = ls.parent_indexer
-local isn = require("luasnip.nodes.snippet").ISN
-local psn = require("luasnip.nodes.snippet").PSN
-local l = require'luasnip.extras'.l
-local r = require'luasnip.extras'.rep
-local p = require("luasnip.extras").partial
-local types = require("luasnip.util.types")
-local events = require("luasnip.util.events")
-local su = require("luasnip.util.util")
+
+require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "./snippets" } })
 
 -- Config
-
 ls.config.set_config {
-  -- This tells LuaSnip to remember to keep around the last snippet.
-  -- You can jump back into it even if you move outside of the selection
   history = false,
-
-  -- This one is cool cause if you have dynamic snippets, it updates as you type!
   updateevents = "TextChanged,TextChangedI",
-
-  -- Autosnippets:
   enable_autosnippets = true,
 }
-
