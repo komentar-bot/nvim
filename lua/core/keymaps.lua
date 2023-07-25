a49c93bb87552dae24da0e602134ba479112cfa6
777uliahshafar <777uliahshafar@gmail.com>
2023-07-25 07:18:44 +0700

    troublekeymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,7 +1,7 @@
 local opts = { noremap = true, silent = true }
 
 local function map(m, k, v)
-	vim.keymap.set(m, k, v, { silent = true })
+  vim.keymap.set(m, k, v, { silent = true })
 end
 
 local ls = require "luasnip"
@@ -15,15 +15,14 @@ map("n", "<leader>sl", "<CMD>SessionLoad<CR>")
 
 map("n", "<f5>", "<CMD>ToggleBackground<CR>")
 
-
--- maps for go down and up 
+-- maps for go down and up
 map("n", "j", "gj")
 map("n", "k", "gk")
 
 -- maps for  spellcheck
-map("n", "<leader>o" , ":setlocal spell! spelllang=id<CR>")
+map("n", "<leader>o", ":setlocal spell! spelllang=id<CR>")
 
-map("n", "<F7>" , ":setlocal spell! spelllang=en_us<CR>")
+map("n", "<F7>", ":setlocal spell! spelllang=en_us<CR>")
 
 -- maps split navigation
 map("n", "<c-h>", "<c-w>h")
@@ -32,19 +31,17 @@ map("n", "<c-k>", "<c-w>k")
 map("n", "<c-l>", "<c-w>l")
 
 -- map buffer
-map("n", "<c-b>", ":ls<CR>:b<Space>", opts )
-map("n", "<leader>h", ":bprevious<CR>", opts )
-map("n", "<leader>l", ":bnext<CR>", opts )
+map("n", "<c-b>", ":ls<CR>:b<Space>", opts)
+map("n", "<leader>h", ":bprevious<CR>", opts)
+map("n", "<leader>l", ":bnext<CR>", opts)
 
 map("n", "<leader><esc>", ":nohlsearch<CR>", opts)
 
---  navigate through popup pum menu  
+--  navigate through popup pum menu
 --vim.api.nvim_set_keymap('i', '<Esc>', 'pumvisible() ? "\\<C-e>" : "\\<Esc>"', {expr = true, noremap = true})
-vim.api.nvim_set_keymap('i', '<CR>', 'pumvisible() ? "\\<C-y>" : "\\<c-g>u<CR>"', {expr = true, noremap = true})
-vim.api.nvim_set_keymap('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true, noremap = true})
-vim.api.nvim_set_keymap('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true, noremap = true})
-
-
+vim.api.nvim_set_keymap("i", "<CR>", 'pumvisible() ? "\\<C-y>" : "\\<c-g>u<CR>"', { expr = true, noremap = true })
+vim.api.nvim_set_keymap("i", "<Tab>", 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', { expr = true, noremap = true })
+vim.api.nvim_set_keymap("i", "<S-Tab>", 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', { expr = true, noremap = true })
 
 ---------------------------------------------
 -- PLUGIN KEYBINDING
@@ -56,7 +53,6 @@ map("n", "<leader>ff", "<CMD>Telescope find_files<CR>")
 map("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
 map("n", "<leader>fw", "<CMD>Telescope live_grep<CR>")
 map("n", "<leader>ht", "<CMD>Telescope colorscheme<CR>")
-
 
 -- nvim-tree
 map("n", "<leader>e", require("nvim-tree.api").tree.toggle)
@@ -89,3 +85,6 @@ map("i", "<c-j>", function()
     ls.change_choice(1)
   end
 end)
+
+-- trouble
+map("n", "<leader>tr", ":TroubleToggle<cr>")
