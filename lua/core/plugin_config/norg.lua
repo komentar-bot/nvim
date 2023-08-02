e1927c66584c41ac33aa3b70cf1e805f67ff01c4
777uliahshafar <777uliahshafar@gmail.com>
2023-08-02 12:04:57 +0800

    todocycle

--- lua/core/plugin_config/norg.lua
+++ lua/core/plugin_config/norg.lua
@@ -73,7 +73,7 @@ require("neorg").setup {
         hook = function(keybinds)
           keybinds.remap_event("norg", "n", "<leader>s", "core.integrations.telescope.find_linkable")
           keybinds.remap_event("norg", "i", "<c-s>", "core.integrations.telescope.insert_link")
-          keybinds.remap_event("norg", "n", "<c-space>", "core.qol.todo_items.todo.task_cycle")
+          keybinds.remap_event("norg", "n", "<leader>c", "core.qol.todo_items.todo.task_cycle")
         end,
       },
     },
