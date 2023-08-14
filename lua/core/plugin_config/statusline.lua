7b3b2420dbe5c43dc68e24193814252efce375d8
777uliahshafar <777uliahshafar@gmail.com>
2023-08-14 10:17:32 +0800

    refinelualine

--- lua/core/plugin_config/statusline.lua
+++ lua/core/plugin_config/statusline.lua
@@ -7,16 +7,16 @@ local lualine = require "lualine"
 -- stylua: ignore
 local colors = {
   bg       = '#202328',
-  fg       = '#bbc2cf',
+  fg       = '#193549',
   yellow   = '#ECBE7B',
   cyan     = '#008080',
   darkblue = '#081633',
-  green    = '#98be65',
+  green    = '#79740e',
   orange   = '#FF8800',
   violet   = '#a9a1e1',
-  magenta  = '#c678dd',
-  blue     = '#51afef',
-  red      = '#ec5f67',
+  magenta  = '#8f3f71',
+  blue     = '#076678',
+  red      = '#9d0006',
   dark4    = '#a89984',
   choco    = '#bdae93',
 }
@@ -45,7 +45,7 @@ local config = {
       -- We are going to use lualine_c an lualine_x as left and
       -- right section. Both are highlighted by c theme .  So we
       -- are just setting default looks o statusline
-      normal = { c = { fg = colors.fg, bg = colors.bg } },
+      normal = { c = { fg = colors.fg, bg = colors.dark4 } },
       inactive = { c = { fg = colors.choco, bg = nil } },
     },
   },
@@ -125,10 +125,10 @@ ins_left {
 ins_left {
   "filename",
   cond = conditions.buffer_not_empty,
-  color = { fg = colors.magenta, gui = "bold" },
+  color = { fg = colors.fg, gui = "bold" },
 }
 
-ins_left { "progress", color = { fg = colors.dark4, gui = "bold" } }
+ins_left { "progress", color = { fg = colors.fg, gui = "bold" } }
 
 ins_left {
   "diagnostics",
@@ -152,7 +152,7 @@ ins_right {
 ins_right {
   "branch",
   icon = "",
-  color = { fg = colors.dark4, gui = "bold" },
+  color = { fg = colors.fg, gui = "bold" },
 }
 
 ins_right {
