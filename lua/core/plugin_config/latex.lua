a9e6a4705d7d14e540c410ed6d0f75659f0cd3bc
777uliahshafar <777uliahshafar@gmail.com>
2023-08-19 06:54:20 +0800

    texcompilemap

--- lua/core/plugin_config/latex.lua
+++ lua/core/plugin_config/latex.lua
@@ -30,7 +30,7 @@ g.vimtex_log_ignore = {
 }
 
 -- maps
-vim.api.nvim_buf_set_keymap(0, "n", "<leader>lc", "<plug>(vimtex-compile)", { noremap = false })
+vim.api.nvim_buf_set_keymap(0, "n", "<leader>ll", "<plug>(vimtex-compile)", { noremap = false })
 vim.api.nvim_buf_set_keymap(0, "n", "<leader>le", "<plug>(vimtex-errors)", { noremap = false })
 vim.api.nvim_buf_set_keymap(0, "n", "<leader>lv", "<plug>(vimtex-view)", { noremap = false })
 vim.api.nvim_buf_set_keymap(0, "n", "<leader>ls", "<plug>(vimtex-toggle-main)", { noremap = false })
