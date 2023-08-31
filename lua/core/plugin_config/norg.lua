4017f1d54f94d22aff279821143c7474fff3bd52
777uliahshafar <777uliahshafar@gmail.com>
2023-08-31 16:52:09 +0800

    mapnextheadingnorg

--- lua/core/plugin_config/norg.lua
+++ lua/core/plugin_config/norg.lua
@@ -98,6 +98,8 @@ require("neorg").setup {
     ["core.keybinds"] = {
       config = {
         hook = function(keybinds)
+          keybinds.remap_event("norg", "n", "<Tab>", "core.integrations.treesitter.next.heading")
+          keybinds.remap_event("norg", "n", "<S-Tab>", "core.integrations.treesitter.previous.heading")
           keybinds.remap_event("norg", "n", "<leader>s", "core.integrations.telescope.find_linkable")
           keybinds.remap_event("norg", "i", "<c-s>", "core.integrations.telescope.insert_link")
           keybinds.remap_event("norg", "n", "<leader>c", "core.qol.todo_items.todo.task_cycle")
