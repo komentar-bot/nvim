815bfa55c57973f8e7934a532c08dc6e7cb6819c
777uliahshafar <777uliahshafar@gmail.com>
2023-09-03 18:02:55 +0800

    norgtocmap

--- lua/core/plugin_config/norg.lua
+++ lua/core/plugin_config/norg.lua
@@ -103,8 +103,8 @@ require("neorg").setup {
           keybinds.remap_event("norg", "n", "<leader>s", "core.integrations.telescope.find_linkable")
           keybinds.remap_event("norg", "i", "<c-s>", "core.integrations.telescope.insert_link")
           keybinds.remap_event("norg", "n", "<leader>c", "core.qol.todo_items.todo.task_cycle")
-          keybinds.map("norg", "n", "<leader>q", "<cmd>Neorg toc qflist<CR>")
-          keybinds.map("norg", "n", "<leader>v", "<cmd>Neorg presenter start<CR>")
+          keybinds.map("norg", "n", "<leader>wt", "<cmd>Neorg toc qflist<CR>")
+          keybinds.map("norg", "n", "<leader>ws", "<cmd>Neorg presenter start<CR>")
         end,
       },
     },
