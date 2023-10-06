fa9a15e49906ae3398ec056436a58c0369448e40
777uliahshafar <777uliahshafar@gmail.com>
2023-10-06 16:51:51 +0800

    mapstampdate

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -8,6 +8,7 @@ require("cmp").setup.buffer {
   },
 }
 
+vim.api.nvim_buf_set_keymap(0, "n", "<leader>d", "<CMD>put =strftime('* %m-%d')<CR>", { noremap = false })
 vim.api.nvim_buf_set_keymap(0, "n", "<leader>k", "<s-{>", { noremap = false })
 vim.api.nvim_buf_set_keymap(0, "n", "<leader>j", "<s-}>", { noremap = false })
 vim.api.nvim_buf_set_keymap(0, "n", "<BS>", "<c-6>", { noremap = true })
