ee4a7521b8c99870f066fa03af30ecc14caa92a5
777uliahshafar <777uliahshafar@gmail.com>
2023-11-26 11:52:23 +0800

    pathsnip

--- lua/core/plugin_config/luasnip.lua
+++ lua/core/plugin_config/luasnip.lua
@@ -4,6 +4,7 @@ end
 
 local ls = require "luasnip"
 
+require("luasnip.loaders.from_lua").load { paths = "~/Appdata/Local/nvim/snippets/" }
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_snipmate").lazy_load()
 
