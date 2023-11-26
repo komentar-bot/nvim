418722420562fdf80ee325518cbd6dac861a0d38
777uliahshafar <777uliahshafar@gmail.com>
2023-11-26 14:46:02 +0800

    cmdsnippetedit

--- lua/core/plugin_config/luasnip.lua
+++ lua/core/plugin_config/luasnip.lua
@@ -9,6 +9,8 @@ require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_snipmate").lazy_load()
 require("luasnip").config.setup { store_selection_keys = "<A-p>" }
 
+vim.cmd [[command! LuaSnipEdit :lua require("luasnip.loaders.from_lua").edit_snippet_files()]] --}}}
+
 -- Config
 ls.config.set_config {
   history = true, --keep around last snippet local to jump back
