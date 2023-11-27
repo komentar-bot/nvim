4664992bac8a1171a88befa019b07468a33a79e7
777uliahshafar <777uliahshafar@gmail.com>
2023-11-27 17:03:24 +0800

    framep

--- snippets/tex.lua
+++ snippets/tex.lua
@@ -5,7 +5,7 @@ local t = ls.text_node
 local i = ls.insert_node
 local sn = ls.snippet_node
 local f = ls.function_node
-local choice = ls.choice_node
+local c = ls.choice_node
 local dynamicn = ls.dynamic_node
 
 local fmt = require("luasnip.extras.fmt").fmt
@@ -22,7 +22,7 @@ ls.add_snippets("tex", {
         return snip.env.TM_SELECTED_TEXT or {}
       end, {}),
     })
-  ),
+  ), --end of snip
 
   s(
     "posp",
@@ -35,7 +35,7 @@ ls.add_snippets("tex", {
         return snip.env.TM_SELECTED_TEXT or {}
       end, {}),
     })
-  ),
+  ), --end of snip
   s(
     "ovop",
     fmt("\\only<{}>{{{}}}", {
@@ -46,7 +46,7 @@ ls.add_snippets("tex", {
         return snip.env.TM_SELECTED_TEXT or {}
       end, {}),
     })
-  ),
+  ), --end of snip
   s(
     "ovsp",
     fmt("\\onslide<{}>{{{}}}", {
@@ -57,7 +57,7 @@ ls.add_snippets("tex", {
         return snip.env.TM_SELECTED_TEXT or {}
       end, {}),
     })
-  ),
+  ), --end of snip
   s(
     "notep",
     fmt("\\notes{{{}}}", {
@@ -67,5 +67,17 @@ ls.add_snippets("tex", {
         return snip.env.TM_SELECTED_TEXT or {}
       end, {}),
     })
-  ),
+  ), --end of snip
+  s(
+    "framep",
+    fmt("\\begin{{frame}}[{},mybg={},mytitle={},mycolor={},{}]\\frametitle{{{}}}{}\\end{{frame}}", {
+      i(1, "c"),
+      c(2, { t "placeholder", t "" }),
+      c(3, { t "standard", t "imageplus", t "center" }),
+      c(4, { t "digiPH_gray", t "digiPH_leaf", t "digiPH_ocean" }),
+      c(5, { t "light", t "dark" }),
+      i(6, "notitle"),
+      i(7, "content"),
+    })
+  ), --end of snip
 }) -- end all
