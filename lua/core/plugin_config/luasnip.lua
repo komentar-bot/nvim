6dfe8d8fe8c88741de1744befca14d76fd8b8a09
777uliahshafar <777uliahshafar@gmail.com>
2023-12-08 16:47:03 -0800

    storesave

--- lua/core/plugin_config/luasnip.lua
+++ lua/core/plugin_config/luasnip.lua
@@ -4,10 +4,9 @@ end
 
 local ls = require "luasnip"
 
-require("luasnip.loaders.from_lua").load { paths = "~/Appdata/Local/nvim/snippets/" }
+require("luasnip.loaders.from_lua").load { paths = "~/AppData/Local/nvim/snippets" }
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_snipmate").lazy_load()
-require("luasnip").config.setup { store_selection_keys = "<Tab>" }
 
 vim.cmd [[command! LuaSnipEdit :lua require("luasnip.loaders.from_lua").edit_snippet_files()]] --}}}
 
@@ -16,4 +15,5 @@ ls.config.set_config {
   history = true, --keep around last snippet local to jump back
   updateevents = "TextChanged,TextChangedI", --update changes as you type
   enable_autosnippets = true,
+  store_selection_keys = "<Tab>",
 } --}}}-
