9f02f912a97efca858b4f72a0ad4ae3ecda14547
777uliahshafar <777uliahshafar@gmail.com>
2023-12-08 18:51:36 -0800

    addbacksnipefld

--- lua/core/plugin_config/luasnip.lua
+++ lua/core/plugin_config/luasnip.lua
@@ -5,9 +5,9 @@ end
 local ls = require "luasnip"
 local snippets_folder = vim.fn.stdpath "config" .. "/snippets/"
 
+require("luasnip.loaders.from_lua").load { paths = snippets_folder }
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_snipmate").lazy_load()
-require("luasnip.loaders.from_lua").load { paths = snippets_folder }
 
 vim.cmd [[command! LuaSnipEdit :lua require("luasnip.loaders.from_lua").edit_snippet_files()]]
 
